ed36a5ef5eb424f581703421a6330cc661143848
Dayronaguilera <aguilera.dayron@gmail.com>
2021-06-07 23:10:36 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 // bottone che genera il prezzo
 var generatePrice = document.getElementsByClassName("create-boxes")[0];
-
 // il prezzo generato 
 var outputPrice = document.getElementById("price-burger");
+
 var discountCupon = 10;
 var userCupon = document.getElementsByClassName("discount")[0];
 
 generatePrice.addEventListener('click', function () { 
-    var resultSumInt = 0 + 50;
+    var resultSumInt = 25;
     var check = document.getElementsByClassName("check-ingre");
     for (var i = 0; i < check.length; i++){
         if (check[i].checked){
             resultSumInt += parseInt(check[i].value)
-        }
-        if (userCupon.value.length === 6){
-            resultSumInt -= resultSumInt * discountCupon / 100;
-
-        }
+        }    
     }
-    
+    if (userCupon.value.length === 6){
+        resultSumInt -= resultSumInt * discountCupon / 100;
+
+    }
     outputPrice.innerHTML = "$ "  + resultSumInt.toFixed(2);
    })
