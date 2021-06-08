c9258d976968875eb1c267b307f6bc974b1fdc3b
Dayronaguilera <aguilera.dayron@gmail.com>
2021-06-08 16:28:19 +0200

    End Esercizio

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
+var userBurger = prompt("Inserisci il nome del tuo Panino: ");
+var nameBurger = document.getElementsByClassName("burgherName")[0];
+
+nameBurger.innerHTML = userBurger;
+
 // bottone che genera il prezzo
 var generatePrice = document.getElementsByClassName("create-boxes")[0];
 // il prezzo generato 
 var outputPrice = document.getElementById("price-burger");
 
-var discountCupon = 10;
 var userCupon = document.getElementsByClassName("discount")[0];
 
-generatePrice.addEventListener('click', function () { 
+generatePrice.addEventListener('click', function() { 
+    // costo del panino + ingredienti!
     var resultSumInt = 25;
     var check = document.getElementsByClassName("check-ingre");
+
     for (var i = 0; i < check.length; i++){
         if (check[i].checked){
             resultSumInt += parseInt(check[i].value)
         }    
     }
-    if (userCupon.value.length === 6){
+    var discountCupon = 10;
+    var sconto10 = "sconto10";
+
+    if (userCupon.value.length > 0 && userCupon.value === sconto10){
         resultSumInt -= resultSumInt * discountCupon / 100;
 
     }
